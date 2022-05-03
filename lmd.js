4a8ca9afbdec36a01b5cd948f04d0b0300d009de
InsaneJ0ker <souviksen2016@gmail.com>
2022-05-03 22:48:08 +0530

    added modal for lmd

---
+++ lmd.js
@@ -0,0 +1,13 @@
+const openAss4 = document.getElementById('open-ass4');
+const modal_container_4 = document.getElementById('modal-container-4');
+const input_4 = document.getElementById("modal-input-ass4")
+//const output_4 = document.getElementById("modal-output-ass4");
+const close_4 = document.getElementById('close-4');
+
+openAss4.addEventListener('click', ()=>{
+    modal_container_4.classList.add('show');
+})
+
+close_4.addEventListener('click', ()=>{
+    modal_container_4.classList.remove('show');
+})
